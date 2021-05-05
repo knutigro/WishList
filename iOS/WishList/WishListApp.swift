11634850e16ac402fe930d8f4e70b027864d8f72
Knut Inge Grosland <hei@knutinge.com>
2021-05-05 21:09:56 +0200

    Added basic SwiftUI app

---
+++ iOS/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Knut Inge Grosland on 2021-05-05.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
